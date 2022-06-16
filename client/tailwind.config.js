7c5407e9d12d0c68fd267429919f4c26d32c2921
Mo <81286824+TravisClark@users.noreply.github.com>
2022-06-16 21:38:48 +0700

    Setup redux-toolkit, tailwindCss

---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Roboto", ...fontFamily.sans],
+    },
+    colors: {
+      heavyBlue: "#0060FF",
+      lightBlue: "#3F80FF",
+      lightGray: "#757575",
+      gray: "#AEB2BB",
+    },
+    screens: {
+      sm: "480px",
+      // => @media (min-width: 480px) { Mobile }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1440px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+};
