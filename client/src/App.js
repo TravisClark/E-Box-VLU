9fa86a150beb17e7cb39eb1685656ff92b9fd8f0
Mo <81286824+TravisClark@users.noreply.github.com>
2022-06-18 16:29:08 +0700

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Navbar from './components/Student/Navbar/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar/>
+    
   );
 }
 
