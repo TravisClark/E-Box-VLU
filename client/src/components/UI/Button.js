177ac370b9f92dc59009051ea167a8d9aa1c878a
Mo <81286824+TravisClark@users.noreply.github.com>
2022-06-21 09:41:14 +0700

    Completed Home page

---
+++ client/src/components/UI/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Button(props) {
+  return (
+    <button className={`${props.className} px-10 py-3 font-semibold rounded`}>{props.title}</button>
+  )
+}
+export default Button
