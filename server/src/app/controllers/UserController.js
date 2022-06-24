0f7c3af52d791d350fe2b594bd4861aa5fe71949
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-24 14:21:27 +0700

    complete api view list user,add user,check account

--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -1,5 +1,53 @@
-//[GET] /user/
-export const home = (req, res) => {
-    res.send('Welcome');
+const UserModel = require('../models/UserModel');
+
+class UserController{
+    //[GET] /user/
+    index = async (req, res) => {
+        try{
+            const users = await UserModel.find();
+            res.json(users);
+        }catch(err){
+            console.log(err);
+        }
+        
+    };
+
+    //[POST] /user/api/add_user
+    add_user = async (req, res, next) => {
+        const formData = req.body;
+        var username = formData.username;
+        formData.password = `VLU${username.trim().slice(-5)}`;
+
+        const user = new UserModel(formData);
+        await user.save()
+            .then(() => {
+                res.send({Message: 'Tao tk thanh cong'})
+            })
+            .catch(next);
+
+    }
+
+    //[POST] /user/api/check_account
+    check_account = async (req, res, next) => {    
+        try{
+            const formData = req.body;
+            var username = formData.username;
+            var password = formData.password;
+            UserModel.findOne({
+                username: username,
+                password: password,
+            })
+                .then(data => {
+                    if(data){
+                        res.json('Dang nhap thanh cong');
+                    }else{
+                        res.json({err: 'Tai khoan hoac mat khau khong chinh xac'});
+                    }
+                });
+        }catch(err){
+            console.log(err);
+        }
+    }
 }
 
+module.exports = new UserController();
