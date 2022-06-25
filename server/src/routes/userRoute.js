09cf2c356b03aaae03b27606a572716e7c8b389e
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-25 14:35:33 +0700

    complete change_password and rename route user

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -5,7 +5,7 @@ const userController = require('../app/controllers/UserController');
 
 router.post('/api/login', userController.login);
 router.post('/api/add_user', userController.add_user);
-router.post('/api/change_password', userController.change_password);
+router.put('/api/change_password', userController.change_password);
 router.get('/api/list_users', userController.list_users);
 
 module.exports = router;
