9972517f309a77342ad7d5fe103d29d91f2a49e3
Mo <81286824+TravisClark@users.noreply.github.com>
2022-06-28 17:20:51 +0700

    finish login function

---
+++ client/src/Student/components/UI/TriangleIcon.js
@@ -0,0 +1,43 @@
+import React from "react";
+import classes from './Triangle.module.css'
+function TriangleIcon() {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 512.055 512.055"
+      width="100"
+      className={`${classes.triangle}  mt-30 opacity-75`}
+    >
+      <g>
+        <g>
+          <path fill='#fff'
+            d="M509.775,481.154L275.109,11.82c-7.862-15.724-30.3-15.724-38.162,0L2.28,481.154
+			c-7.092,14.185,3.222,30.874,19.081,30.874h469.333C506.553,512.028,516.868,495.338,509.775,481.154z M55.879,469.361
+			L256.028,69.064l200.149,400.297H55.879z"
+          />
+        </g>
+      </g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+    </svg>
+  );
+}
+
+export default TriangleIcon;
