8ae1a7e20f16dae5620271c65b056595e3283689
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-06-29 12:38:44 +0700

    create Mailbox Model

---
+++ server/src/app/models/MailboxModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+const Schema = mongoose.Schema;
+
+const Mailbox = new Schema(
+    {
+        id_question : {
+            type: Number,
+        },
+        question: {
+            type: 'string',
+            maxlength: 255,
+            required: true,
+        },
+        answer: {
+            type: 'string',
+            maxlength: 255,
+        },
+        status: {
+            type: 'string',
+            maxlength: 255,
+            required: true,
+        },
+        type_name: {
+            type: 'string',
+            maxlength: 255,
+        },
+        user_name_question: {
+            type: 'string',
+            maxlength: 20,
+            required: true,
+        },
+        user_name_censorship: {
+            type: 'string',
+            maxlength: 20,
+            required: true,
+        },
+        user_name_answer: {
+            type: 'string',
+            maxlength: 20,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+User.plugin(AutoIncrement, { inc_field: 'id_question' });
+
+module.exports = mongoose.model('Mailbox', Mailbox);
