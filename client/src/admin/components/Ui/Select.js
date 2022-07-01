f112d02505bab9d7dc64895b08651d69f11eb13a
Mo <81286824+TravisClark@users.noreply.github.com>
2022-07-01 10:21:19 +0700

    Admin setup layout completed

--- client/src/admin/components/Ui/Select.js
+++ client/src/admin/components/Ui/Select.js
@@ -1,90 +1,40 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Controller } from "react-hook-form";
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+export default function BasicSelect(props) {
+  // const [age, setAge] = React.useState("");
 
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
-
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
-export default function MultipleSelectPlaceholder() {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
-
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
-  };
-
+  // const handleChange = (event) => {
+  //   setAge(event.target.value);
+  // };
+  const DUMMY_ROLES = ['admin', 'user', 'assistant']
+  const roles = DUMMY_ROLES.map(role => <MenuItem value={role} key={role}>{role}</MenuItem>)
   return (
-    <div>
-      <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
-        <Select
-          multiple
-          displayEmpty
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput />}
-          renderValue={(selected) => {
-            if (selected.length === 0) {
-              return <em>Placeholder</em>;
-            }
-
-            return selected.join(', ');
-          }}
-          MenuProps={MenuProps}
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem disabled value="">
-            <em>Placeholder</em>
-          </MenuItem>
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Role</InputLabel>
+        <Controller
+          render={({ field }) => (
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              // value={age}
+              label="Role"
+              // onChange={handleChange}
+              {...field}
             >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
+              {roles}
+            </Select>
+          )}
+          control={props.control}
+          name={props.name}
+        />
       </FormControl>
-    </div>
+    </Box>
   );
 }
--- client/src/admin/pages/Users.js
+++ client/src/admin/pages/Users.js
@@ -1,28 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../../student/components/UI/Container";
-import AdminContainer from "../components/AdminContainer/AdminContainer";
-import SideNav from "../components/AdminNav/SideNav/SideNav";
-import Background from "../components/Background/Background";
 
 function Users() {
   return (
-    <>
-      <Background>
-        <SideNav />
-        <Container className="m-auto w-11/12 h-full py-24 px-20 ">
-          <div className="flex justify-between">
-            <h1 className="text-2xl font-semibold ">Câu Hỏi</h1>
-            <Link
-              to="/E-boxVLU/admin/users/add"
-              className="bg-lightBlue px-4 py-2 rounded-xl font-medium text-white"
-            >
-              Add User
-            </Link>
-          </div>
-        </Container>
-      </Background>
-    </>
+    <Container className="m-auto w-11/12 h-full py-24 px-20 ">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold ">Users</h1>
+        <Link
+          to="/E-boxVLU/admin/users/add"
+          className="bg-lightBlue px-4 py-2 rounded-xl font-medium text-white"
+        >
+          Add User
+        </Link>
+      </div>
+    </Container>
   );
 }
 
