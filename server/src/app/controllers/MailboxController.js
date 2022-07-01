517368a6a780aff8c906402fcd902c12725e1bd4
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-07-01 15:13:09 +0700

    complete reply question function in Back-End

--- server/src/app/controllers/MailboxController.js
+++ server/src/app/controllers/MailboxController.js
@@ -43,9 +43,9 @@ class MailboxController {
                     answer: '',
                     status: 'Chưa được duyệt',
                     type_name: '',
-                    user_name_question: data_username,
-                    user_name_censor: '',
-                    user_name_answer: '',
+                    username_question: data_username,
+                    username_censor: '',
+                    username_reply: '',
                 };
                 //Add a new mailbox to the database
                 const mailbox = new Mailbox(info_mailbox);
@@ -73,7 +73,7 @@ class MailboxController {
             var status = 'Đã được duyệt';
             await Mailbox.findOneAndUpdate(
                 { id_question: data_id_question },
-                { status: status, user_name_censor: data_username },
+                { status: status, username_censor: data_username },
             );
             //create informational data for the notification
             const info_mailbox = await Mailbox.findOne({
@@ -83,7 +83,7 @@ class MailboxController {
                 question: info_mailbox.question,
                 notification: status,
                 username_sender: data_username,
-                username_receiver: info_mailbox.user_name_question,
+                username_receiver: info_mailbox.username_question,
             };
             //create notifications for students
             const notification = new Notification(info_notification);
@@ -110,7 +110,7 @@ class MailboxController {
             var status = 'Đã bị từ chối';
             await Mailbox.findOneAndUpdate(
                 { id_question: data_id_question },
-                { status: status, user_name_censor: data_username },
+                { status: status, username_censor: data_username },
             );
             //create informational data for the notification
             const info_mailbox = await Mailbox.findOne({
@@ -120,7 +120,7 @@ class MailboxController {
                 question: info_mailbox.question,
                 notification: status,
                 username_sender: data_username,
-                username_receiver: info_mailbox.user_name_question,
+                username_receiver: info_mailbox.username_question,
             };
             //create notifications for students
             const notification = new Notification(info_notification);
@@ -132,6 +132,68 @@ class MailboxController {
                     });
                 })
                 .catch(next);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //[PATCH] http://localhost:5000/api/mailbox/reply_question
+    reply_question = async (req, res, next) => {
+        try {
+            //Get data from client
+            const data_username = req.body.username;
+            const data_answer = req.body.answer;
+            const data_id_question = req.body.id_question;
+            const data_type_name = req.body.type_name;
+            //format answer
+            var answer = data_answer.replace(/\s+/g, '');
+            if (answer == null || answer === '') {
+                //check answer is null or ''
+                return next(
+                    res.status(401).json({
+                        Message: 'Vui lòng nhập câu trả lời',
+                    }),
+                );
+            } else if (data_type_name == null || data_type_name === '') {
+                //check type name is null or ''
+                return next(
+                    res.status(401).json({
+                        Message: 'Vui lòng chọn thể loại câu hỏi',
+                    }),
+                );
+            } else {
+                //update answer status to MongoDB
+                var status = 'Đã được trả lời';
+                await Mailbox.findOneAndUpdate(
+                    { id_question: data_id_question },
+                    {
+                        status: status,
+                        answer: data_answer,
+                        type_name: data_type_name,
+                        username_reply: data_username,
+                    },
+                );
+                //create informational data for the notification
+                const info_mailbox = await Mailbox.findOne({
+                    id_question: data_id_question,
+                });
+                const info_notification = {
+                    question: info_mailbox.question,
+                    notification: status,
+                    username_sender: data_username,
+                    username_receiver: info_mailbox.username_question,
+                };
+                //create notifications for students
+                const notification = new Notification(info_notification);
+                notification
+                    .save()
+                    .then(() => {
+                        res.status(201).json({
+                            Message: 'Trả lời câu hỏi thành công',
+                        });
+                    })
+                    .catch(next);
+            }
         } catch (err) {
             console.log(err);
         }
--- server/src/routes/mailboxRoute.js
+++ server/src/routes/mailboxRoute.js
@@ -7,5 +7,6 @@ router.post('/publish_question', mailboxController.publish_question);
 router.get('/list_questions', mailboxController.list_questions);
 router.patch('/approve_question', mailboxController.approve_question);
 router.patch('/refuse_question', mailboxController.refuse_question);
+router.patch('/reply_question', mailboxController.reply_question);
 
 module.exports = router;
