4f73e1801e2d83d6d90720f5bf76d113880172f1
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-07-06 12:07:42 +0700

    complete add type and edit add role

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,9 @@ const admin_userRoute = require('./admin_userRoute');
 const user_userRoute = require('./user_userRoute');
 const admin_mailboxRoute = require('./admin_mailboxRoute');
 const user_mailboxRoute = require('./user_mailboxRoute');
+const admin_typeRoute = require('./admin_typeRoute');
+const user_typeRoute = require('./user_typeRoute');
+
 const { checkLogin } = require('../middleware/Auth');
 
 function route(app) {
@@ -11,10 +14,12 @@ function route(app) {
     app.use('/api/admin/role', admin_roleRoute);
     app.use('/api/admin/user', admin_userRoute);
     app.use('/api/admin/mailbox', admin_mailboxRoute);
+    app.use('/api/admin/type', admin_typeRoute);
 
     //routes in user
     app.use('/api/user/user', user_userRoute);
     app.use('/api/user/mailbox', user_mailboxRoute);
+    app.use('/api/user/type', user_typeRoute);
 }
 
 module.exports = route;
