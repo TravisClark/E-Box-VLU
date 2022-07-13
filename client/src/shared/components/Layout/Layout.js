310213acf774e45181faf87bd7190c913046d049
Mo <81286824+TravisClark@users.noreply.github.com>
2022-07-13 21:12:29 +0700

    improve question detail responsive

--- client/src/shared/components/Layout/Layout.js
+++ client/src/shared/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, {  useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { AdminLayout } from "../../../admin/components/AdminLayout/AdminLayout";
@@ -9,19 +9,23 @@ import { uiActions } from "../../store/ui-slice";
 function Layout(props) {
   const dispatch = useDispatch();
   const history = useHistory();
-  const {isInAdminMode} = useSelector((state) => state.ui)
+  const { isInAdminMode } = useSelector((state) => state.ui);
 
   const { isLoggedIn } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(authActions.autoLoginHandler());
-    dispatch(uiActions.runAdminMode({type:'REFRESH_ADMIN_PAGE'}))
-    isInAdminMode && history.replace('/E-boxVLU/admin/dashboard')
+    dispatch(uiActions.runAdminMode({ type: "REFRESH_ADMIN_PAGE" }));
+    isInAdminMode && history.replace("/E-boxVLU/admin/dashboard");
   }, [dispatch, isLoggedIn, history, isInAdminMode]);
 
   return (
     <>
-      {isInAdminMode ? <AdminLayout>{props.children}</AdminLayout> : <StudentLayout>{props.children}</StudentLayout>}
+      {isInAdminMode ? (
+        <AdminLayout>{props.children}</AdminLayout>
+      ) : (
+        <StudentLayout>{props.children}</StudentLayout>
+      )}
     </>
   );
 }
