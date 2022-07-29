692c226702e16baa5894adf6b19a5af2e84216bb
Mo <81286824+TravisClark@users.noreply.github.com>
2022-07-29 18:09:17 +0700

    token check added

--- client/src/shared/store/auth-slice.js
+++ client/src/shared/store/auth-slice.js
@@ -1,30 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import jwt_decode from "jwt-decode";
 const authSlice = createSlice({
     name: 'auth',
-    initialState: {isLoggedIn: false, account: {}, token: ''},
+    initialState: {isLoggedIn: false, account: {}, token: null},
     reducers:{
         loginHandler(state, action) {
-            sessionStorage.setItem('account', JSON.stringify(action.payload))
+            state.token = action.payload.token.token
+            sessionStorage.setItem('token', JSON.stringify(state.token))
             state.isLoggedIn = true;
-            state.account = action.payload
+            state.account = jwt_decode(state.token)
         },
         saveUserHandler(state, action) {
             state.account = action.payload
         },
         autoLoginHandler(state) {
-            state.account = JSON.parse(sessionStorage.getItem('account'));
+            state.token = JSON.parse(sessionStorage.getItem('token'));
+            state.token && (state.account = jwt_decode(state.token))
             state.account && (state.isLoggedIn = true) ;
         },
         logoutHandler(state) {
             state.account = {}
-            sessionStorage.removeItem('account')
+            sessionStorage.removeItem('token')
             state.isLoggedIn = false;
+            state.token = null
         },
         changePasswordHandler(state, action) {
             const {username, password} = action.payload;
             sessionStorage.clear();
-            sessionStorage.setItem('account', JSON.stringify(username, password));
+            sessionStorage.setItem('token', JSON.stringify(username, password));
         }
     }
 })
