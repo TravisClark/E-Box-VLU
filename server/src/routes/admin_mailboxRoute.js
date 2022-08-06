9557df2cb112f50fb7ef3d15f47d80b1f89c8a9d
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2022-08-06 15:03:14 +0700

    complete API view statistics function

--- server/src/routes/admin_mailboxRoute.js
+++ server/src/routes/admin_mailboxRoute.js
@@ -5,6 +5,7 @@ const { checkLogin } = require('../middleware/Auth');
 const mailboxController = require('../app/controllers/MailboxController');
 
 router.get('/list_questions_admin', mailboxController.list_questions_admin);
+router.get('/statistical', mailboxController.statistical);
 router.patch('/approve_question', mailboxController.approve_question);
 router.patch('/refuse_question', mailboxController.refuse_question);
 router.patch('/restore_question', mailboxController.restore_question);
